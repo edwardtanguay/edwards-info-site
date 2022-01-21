b4c2df2b4b521344b978fd69b5b0249edce09a9a
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-21 13:56:24 +0100

    add jamstack link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 			<h1>Edward's Info Site</h1>
 			<ul>
 				<li>Learn React: <a href="https://reactjs.org/docs/getting-started.html">React documentation</a></li>
+				<li>Learn JAMStack: <a href="https://jamstack.org">JAMStack start site</a> </li>
 			</ul>
 		</div>
 	);
