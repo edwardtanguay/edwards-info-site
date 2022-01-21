93b7e9a36042f01186544723763e33251563b262
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-21 13:50:43 +0100

    info site ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Edward's Info Site</h1>
+			<ul>
+				<li>Learn React: <a href="https://reactjs.org/docs/getting-started.html">React documentation</a></li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
