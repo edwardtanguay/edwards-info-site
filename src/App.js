f11fdb8a6f1c15bd2739a2a2cb41b9ccdba75fb9
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-21 15:41:12 +0100

    new products

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 /* eslint-disable jsx-a11y/alt-text */
+import { useEffect, useState } from 'react';
 import './App.scss';
-import data from './data.json';
-
-const siteName = "Edward's Info Site";
+import siteData from './siteData.json';
 
 function App() {
+	const [employees, setEmployees] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch('https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json');
+			const _employees = await response.json();
+			setEmployees(_employees);
+		})();
+	}, []);
+
 	return (
 		<div className="App">
-			<h1>{siteName}</h1>
+			<h1>{siteData.title}</h1>
 			<img src="images/general/profile.jpg" />
+			<h2>Links</h2>
 			<ul>
-				{data.links.map(link => {
+				{siteData.links.map(link => {
 					return (
 						<li className={link.favorite ? 'favorite' : ''}>
 							<div>
@@ -18,9 +28,17 @@ function App() {
 							</div>
 							<img src={`images/links/${link.idCode}.png`} />
 						</li>
-	)
-})}
+					)
+				})}
 			</ul >
+			<h2>{employees.length} Employees</h2>
+			<ul>
+				{employees.map(employee => {
+					return (
+						<li>{employee.firstName} {employee.lastName}</li>
+					)
+				})}
+			</ul>
 		</div >
 	);
 }
