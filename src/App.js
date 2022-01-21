56b187e86a2f5ef808c20dce3a91a81b2fbd2ebf
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-21 14:03:21 +0100

    add dark them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+const siteName = "Edward's Info Site";
+
 function App() {
 	return (
 		<div className="App">
-			<h1>Edward's Info Site</h1>
+			<h1>{siteName}</h1>
 			<ul>
 				<li>Learn React: <a href="https://reactjs.org/docs/getting-started.html">React documentation</a></li>
 				<li>Learn JAMStack: <a href="https://jamstack.org">JAMStack start site</a> </li>
